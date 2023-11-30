d29307de1fe44f56e55409c7cd69a7fbd1c335e4
dartcom <kostas2you@gmail.com>
2023-11-30 22:08:07 +0200

    fixed hirose FPC connector poinitng backwards on the 2 lcd boards+added stm32f401 boilerplate code

---
+++ software/src/main.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../system/stm32f4xx.h"
+
+
+
+int main(){
+    while(1){
+        __NOP();
+        
+    }
+
+    return 0;
+}
