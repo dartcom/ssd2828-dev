22cdc2b123465274b47534701f02c21ede968a4d
dartcom <kostas2you@gmail.com>
2023-12-02 01:41:29 +0200

    added config for cortex debug + added blink for stm32

--- software/src/main.c
+++ software/src/main.c
@@ -4,13 +4,37 @@
 #include <stdlib.h>
 #include "../system/stm32f4xx.h"
 
+void gpio_init(){
+    //enable GPIOC and GPIOA GPIOB GPIOC
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN;
+    //set A0 to input
+    GPIOA->MODER &= ~GPIO_MODER_MODER0_0 & ~GPIO_MODER_MODER0_1;
+    //set C13 to output
+    GPIOC->MODER |= (0x1<<GPIO_MODER_MODER13_Pos);
+    //set A0 with internal pull up
+    GPIOA->PUPDR |= (0x1<<GPIO_PUPDR_PUPD0_Pos);
+    //C13 to push pull
+    GPIOC->OTYPER &= ~GPIO_OTYPER_OT13;
+    //set C13 speed to low
+    GPIOC->OSPEEDR &= ~GPIO_OSPEEDER_OSPEEDR13;
+    //set C13 to LOW
+    GPIOC->ODR |= GPIO_ODR_OD13;
 
+  
+}
 
 int main(){
+    SystemInit();
+    SystemCoreClockUpdate();
+    gpio_init();
+    uint32_t i = 0;
     while(1){
-        __NOP();
-        
-    }
+        i++;
+        if(i == 840000){
+            GPIOC->ODR ^= GPIO_ODR_OD13;
+            i = 0;
+        }
 
+    }
     return 0;
 }
